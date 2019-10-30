3398f184abbcfbac35eb9be913748b67ed58493c
yukimakura <yukimakura86@gmail.com>
2019-10-31 00:22:56 +0900

    mutex修正

--- Src/udp_easy_io.c
+++ Src/udp_easy_io.c
@@ -13,21 +13,21 @@ static unsigned short dst_port_;
 static unsigned short own_port_;
 struct udp_pcb *pcb_;
 err_t err_;
-static char udp_read_buffer_[128];
+static char udp_read_buffer_[UDP_READ_BUFFER_SIZE];
+static char mutex_char_buff_[UDP_READ_BUFFER_SIZE];
 static unsigned char read_flag_;
-static char *empty_str_ = "";
 
-static osMutexId_t myMutexEthrHandle;
+static osMutexId_t myMutexEthrHandle_;
 
 static void UDP_EASY_IO_recv_(void *arg, struct udp_pcb *pcb, struct pbuf *p,
 		const ip_addr_t *addr, u16_t port) {
-	char printbuff[64] = "";
 	if (p != NULL) {
-		osMutexAcquire(myMutexEthrHandle, osWaitForever);
-		memset(udp_read_buffer_,'\0',sizeof(udp_read_buffer_));//buffer clear
-		memcpy(udp_read_buffer_,p->payload,p->len);
-		read_flag_ = 0;
-		osMutexRelease(myMutexEthrHandle);
+		if (osMutexAcquire(myMutexEthrHandle_, osWaitForever) == osOK){
+			memset(udp_read_buffer_,'\0',sizeof(udp_read_buffer_));//buffer clear
+			memcpy(udp_read_buffer_,p->payload,p->len);
+			read_flag_ = 0;
+			osMutexRelease(myMutexEthrHandle_);
+		}
 	}
 }
 
@@ -50,19 +50,22 @@ void UDP_EASY_IO_write(char *payload) {
 
 	struct pbuf *p;
 
-	osMutexAcquire(myMutexEthrHandle, osWaitForever);
 	p = pbuf_alloc(PBUF_TRANSPORT, strlen(payload), PBUF_RAM);
 	memcpy(p->payload, payload, strlen(payload));
 	udp_sendto(pcb_, p, &dst_addr_, dst_port_);
 	pbuf_free(p);
-	osMutexRelease(myMutexEthrHandle);
 }
 unsigned char UDP_EASY_IO_was_read() {
 	return read_flag_;
 }
 char *UDP_EASY_IO_read_char_prt() {
 		read_flag_ = 1;
-		return udp_read_buffer_;
+		sprintf(mutex_char_buff_,"");
+		if (osMutexAcquire(myMutexEthrHandle_, osWaitForever) == osOK){
+			memcpy(mutex_char_buff_,udp_read_buffer_,strlen(udp_read_buffer_));
+			osMutexRelease(myMutexEthrHandle_);
+		}
+		return mutex_char_buff_;
 }
 
 char UDP_EASY_IO_read_char() {
@@ -80,9 +83,13 @@ void UDP_EASY_IO_init(ip4_addr_t dst_addr, unsigned short dst_port,
 	/* Create the mutex(es) */
 	/* definition and creation of myMutexEthr */
 	const osMutexAttr_t myMutexEthr_attributes = {
-			.name = "myMutexEthr",
+	  "myMutexEthr",
+	  osMutexRecursive,
+	  NULL,
+	  0U
 	};
-	myMutexEthrHandle = osMutexNew(&myMutexEthr_attributes);
+
+	myMutexEthrHandle_ = osMutexNew(&myMutexEthr_attributes);
 
 	xTaskCreate(&UDP_EASY_IO_main_task_ , "UDP_EASY_IO_main_task", 512, NULL, 1, NULL);
 }
